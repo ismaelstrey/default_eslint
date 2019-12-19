5a89fab52f973eb83a84b39ed29ab4fac8a38c32
Ismael Strey <imsaletsrey@hotmail.com>
2019-12-19 09:44:48 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from './Button'
+
+const App = () => {
+  return (
+    <div>
+      <p>Digital Innovation One</p>
+      <p>Bem Vindo a nossa Aula</p>
+      <div>
+        <Button />
+      </div>
+    </div>
+  )
+}
+export default App
